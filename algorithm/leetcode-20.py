0cde4ab2285a76ee56daa52e1e021591ad4a0ad1
PotentialPie <823974678@qq.com>
2019-02-18 12:55:41 +0800

    update 216

--- algorithm/leetcode-20.py
+++ algorithm/leetcode-20.py
@@ -10,25 +10,30 @@ class Solution:
 
         stack = []
         com_dict = {'(':')', '[':']', '{':'}'}
-        head_index = -1
         for str_index in range(len(s)):
-            # 这种问题，千万别搞错了，不是==，是看匹不匹配，所以上一个必须有限制，并且用字典来确认
-            if not stack or stack[head_index] not in ['(', '[', '{'] or com_dict[stack[head_index]] != s[str_index]:
-                #print(str_index)
-                # 如果想不用pop的话，这里不应该用append，应该是stack[index+1] = value
-                # 但是要提前定义数组长度，所以，还是pop好！！！
+            # # 这种问题，千万别搞错了，不是==，是看匹不匹配，所以上一个必须有限制，并且用字典来确认
+            # if not stack or stack[head_index] not in ['(', '[', '{'] or com_dict[stack[head_index]] != s[str_index]:
+            #     #print(str_index)
+            #     # 如果想不用pop的话，这里不应该用append，应该是stack[index+1] = value
+            #     # 但是要提前定义数组长度，所以，还是pop好！！！
+            #     stack.append(s[str_index])
+            #     head_index = head_index + 1
+            #     #print(stack, head_index)
+            # else:
+            #     #print(str_index)
+            #     # 要把元素删除掉啊,pop默认是删除最后一个元素
+            #     stack.pop()
+            #     head_index = head_index - 1
+            if s[str_index] in ['(', '[', '{']:
                 stack.append(s[str_index])
-                head_index = head_index + 1
-                #print(stack, head_index)
             else:
-                #print(str_index)
-                # 要把元素删除掉啊,pop默认是删除最后一个元素
+                if not stack or com_dict[stack[-1]] != s[str_index]:
+                    return False
                 stack.pop()
-                head_index = head_index - 1
-        if head_index == -1:
-            return True
-        else:
+        if stack:
             return False
+        else:
+            return True
 
 solution = Solution()
 print(solution.isValid("()"))
